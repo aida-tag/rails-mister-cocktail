121ed7cefa3dc661381770e1b4aa8c9d9c8fcef4
aida tagmouti <aida.tagmouti@gmail.com>
2020-11-12 17:05:17 +0100

    cocktail app design ongoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'mint leaves')
+
+Cocktail.create(name: 'Whisky Sour')
+Cocktail.create(name: 'Old Fashioned')
+
+Dose.create(description: '2cl', ingredient_id: 1, cocktail_id: 1)
+Dose.create(description: '1,5oz', ingredient_id: 1, cocktail_id: 1)
+Dose.create(description: '6 leaves', ingredient_id: 3, cocktail_id: 2)
